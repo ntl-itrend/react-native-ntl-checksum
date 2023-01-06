3dbed8ca4d86a63412fe8686c359308bf5ddba3a
Suradis.S <suradis.workspace@gmail.com>
2023-01-06 13:31:51 +0700

    feat: init android

--- android/src/main/java/com/ntlchecksum/NtlChecksumModule.kt
+++ android/src/main/java/com/ntlchecksum/NtlChecksumModule.kt
@@ -1,8 +1,11 @@
 package com.ntlchecksum
 
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import java.io.InputStream
+import java.security.MessageDigest
+
 
 class NtlChecksumModule internal constructor(context: ReactApplicationContext) :
   NtlChecksumSpec(context) {
@@ -13,9 +16,87 @@ class NtlChecksumModule internal constructor(context: ReactApplicationContext) :
 
   // Example method
   // See https://reactnative.dev/docs/native-modules-android
+  // @ReactMethod
+  // override fun multiply(a: Double, b: Double, promise: Promise) {
+  //   promise.resolve(a * b)
+  // }
+
   @ReactMethod
-  override fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
+  override fun getChecksum(promise: Promise) {
+    val hash: ByteArray
+    val sb = StringBuilder()
+    try {
+      val file_name = "index.android.bundle"
+      val assetManager = getReactApplicationContext().getAssets()
+      val stream: InputStream = assetManager.open(file_name)
+      if (stream == null) {
+        promise.resolve("");
+        return;
+      }
+      val size: Int = stream.available()
+      val buffer = stream.readBytes()
+      hash = MessageDigest.getInstance("SHA-256").digest(buffer)
+      if(hash == null) {
+        promise.resolve("")
+        return
+      }
+      val md: MessageDigest
+      for(i in hash) {
+        sb.append(Integer.toString((i.toInt() and 0xff)+ 0x100, 16).substring(1))
+      }
+      promise.resolve(sb.toString())
+      return
+    } catch (e: Exception) {
+      promise.resolve("")
+    }
+    // try {
+
+    // } catch(Exception e){
+
+    // }
+      //  byte[] hash = null;
+      //   StringBuilder sb = new StringBuilder();
+      //   try {
+      //       AssetManager assetManager = getReactApplicationContext().getAssets();
+      //       InputStream stream = assetManager.open("index.android.bundle");
+      //       if (stream == null) {
+      //           promise.resolve("");
+      //           return;
+      //       }
+      //       int size = stream.available();
+      //       byte[] buffer = new byte[size];
+      //       stream.read(buffer);
+      //       stream.close();
+      //       hash = MessageDigest.getInstance("SHA-256").digest(buffer);
+      //       if (hash == null) {
+      //           promise.resolve("");
+      //           return;
+      //       }
+
+      //   } catch (Exception ex) {
+      //       ex.printStackTrace();
+      //       promise.resolve("");
+      //       return;
+      //   }
+
+      //   MessageDigest md = null;
+      //   try {
+      //       for (int i = 0; i < hash.length; i++) {
+      //           sb.append(Integer.toString((hash[i] & 0xff) + 0x100, 16).substring(1));
+      //       }
+
+      //       promise.resolve(sb.toString());
+      //       return;
+      //   } catch (Exception e) {
+      //       e.printStackTrace();
+      //       promise.resolve("");
+      //       return;
+      //   }
+  }
+
+  @ReactMethod
+  override fun getChecksumCert(name: String, promise: Promise) {
+    promise.resolve("cert")
   }
 
   companion object {
--- android/src/oldarch/NtlChecksumSpec.kt
+++ android/src/oldarch/NtlChecksumSpec.kt
@@ -7,5 +7,7 @@ import com.facebook.react.bridge.Promise
 abstract class NtlChecksumSpec internal constructor(context: ReactApplicationContext) :
   ReactContextBaseJavaModule(context) {
 
-  abstract fun multiply(a: Double, b: Double, promise: Promise)
+  // abstract fun multiply(a: Double, b: Double, promise: Promise)
+  abstract fun getChecksum(promise: Promise)
+  abstract fun getChecksumCert(name: String, promise: Promise)
 }
